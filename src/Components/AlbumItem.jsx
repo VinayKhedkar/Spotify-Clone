0e7170c9aaad6aed67504753a1e4fd66ac1f43b4
VinayKhedkar <arithmetic.chip@gmail.com>
2024-05-22 18:05:18 +0530

    Minor changes in image sizes

--- src/Components/AlbumItem.jsx
+++ src/Components/AlbumItem.jsx
@@ -4,7 +4,7 @@ function AlbumItem({id, name, desc, image, bgColor}){
 
     const navigate = useNavigate()
     return (
-        <div onClick={(()=>navigate(`/album/${id}`))} className={`bg-[${bgColor}] p-2 flex flex-col justify-center w-[12vw] min-w-[100px] rounded hover:bg-[#242424] hover:cursor-pointer`}>
+        <div onClick={(()=>navigate(`/album/${id}`))} className={`bg-[${bgColor}] p-2 flex flex-col justify-center w-[12vw] min-w-[170px] rounded hover:bg-[#242424] hover:cursor-pointer`}>
             <img className="rounded" src={image} alt="" />
             <p className="text-lg text-white font-bold">{name}</p>
             <p className="text-sm">{desc}</p>
--- src/Components/SongItem.jsx
+++ src/Components/SongItem.jsx
@@ -5,7 +5,7 @@ function SongItem({id, name, desc, image, duration}){
 
     const {playWithId} = useContext(PlayerContext)
     return (
-        <div onClick={() => playWithId(id)} className='p-2 flex flex-col justify-center w-[12vw] min-w-[100px] rounded-xl hover:bg-[#242424] cursor-pointer'>
+        <div onClick={() => playWithId(id)} className='p-2 flex flex-col justify-center w-[12vw] min-w-[170px] rounded-xl hover:bg-[#242424] cursor-pointer'>
             <img className="rounded" src={image} alt="" />
             <p className="text-lg text-white font-bold">{name}</p>
             <p className="text-sm">{desc}</p>
